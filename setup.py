cef0a5a084387ba8795169caa709168a0d08c187
abhi-kumar-123 <abhishekkumar123067@gmail.com>
2025-12-05 12:14:22 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Generativ AI Project',
+    version='0.0.0',
+    author='Abhishek Kumar',
+    author_email='abhishekkumar123067@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
